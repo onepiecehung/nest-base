2d7c57a90d4862382fc2d65d05046bd9f9ce999e
Nguyễn Hữu Hưng <360anime.suki@gmail.com>
2022-11-30 16:18:35 +0700

    aa

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,12 +1,12 @@
 export const firebaseConfig = {
-  type: ''
-  project_id: ''
+  type: '',
+  project_id: '',
   private_key_id: '',
-  private_key:'',
+  private_key: '',
   client_email: '',
   client_id: '',
   auth_uri: '',
   token_uri: '',
   auth_provider_x509_cert_url: '',
-  client_x509_cert_url:''
+  client_x509_cert_url: '',
 };
